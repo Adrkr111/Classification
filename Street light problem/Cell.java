6ab2190f67df26b2521e0be6d4cf20bc805ec46f
Adrkr111 <58401097+Adrkr111@users.noreply.github.com>
2020-03-24 12:16:10 +0530

    Update Cell.java

--- Street light problem/Cell.java
+++ Street light problem/Cell.java
@@ -4,17 +4,16 @@ import java.util.stream.IntStream;
 public class Cell{
              public static void main(String args[])
              throws IOException{
-             BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-             HashSet<String> set=new HashSet<String>();
-             String s=br.readLine();
-             s=s.replace("(","");
-             s=s.replace(")","");
-             String bf[]=s.split(",");
-             int []n=new int[bf.length];
-             for(int i=0;i<bf.length;i+=2){
-				 IntStream st=IntStream.range(Integer.parseInt(bf[i]),Integer.parseInt(bf[i+1]));
-				 st.forEach((j)->{set.add(Integer.toString(j));});
+             BufferedReader br=new BufferedReader(new InputStreamReader(System.in));//Creating BufferedReader object
+             HashSet<String> set=new HashSet<String>(); //Creating a Set object
+             String s=br.readLine(); //Creating String obj
+             s=s.replace("(",""); //(RP)Replacing Operations For Extracting Data from formated input.
+             s=s.replace(")","");//(RP)
+             String bf[]=s.split(","); //Creating an array of useful data.
+             for(int i=0;i<bf.length;i+=2){    //To take pair of numbers into consideration.(Each pair corrsponds to a single streetlight having a starting and ending street number}  )
+				 IntStream st=IntStream.range(Integer.parseInt(bf[i]),Integer.parseInt(bf[i+1]));//creating stream of numbers(generating all the streets numbers covered by each streetlight)
+				 st.forEach((j)->{set.add(Integer.toString(j));});//only unique values are added to the set which are not encountered earlier(New streets numbers are added to set)
 			 }
-              System.out.println(""+set.size());
+              System.out.println(""+set.size());//total number of streets lightup.
              }
              }
